beb2534cc95da54cfa3ef2fa853453b4c077fe91
naufal55 <moehhammadnofall55@gmail.com>
2021-09-05 13:57:14 +0700

    inisialisasi project MERN

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import { Routes } from '../config';
+import logo from './../assets/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Register from "./register";
+import Home from "./home";
+import Login from './login'
+
+export {Register,Home,Login};
